435918d52bb2285210b28b90ba4e5545a44fd5e0
Lucas Gabriel de Paula Pinto <lucas.gabriellgpp@gmail.com>
2025-12-29 02:09:09 -0300

    Adicionar Estrutura e Estilo

---
+++ src/Components/JogoDaVelha/JogoDaVelha.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import './JogoDaVelha.css';
+import circle_icon from '../Assets/circle.png';
+import cross_icon from '../Assets/cross.png';
+
+const JogoDaVelha = () => {
+  return (
+    <div className='container'>
+        <h1 className="title">Jogo da Velha em <span>React</span></h1>
+        <div className="board">
+            <div className="row1">
+                <div className="boxes"></div>
+                <div className="boxes"></div>
+                <div className="boxes"></div>
+            </div>
+            <div className="row2">
+                <div className="boxes"></div>
+                <div className="boxes"></div>
+                <div className="boxes"></div>
+            </div>
+            <div className="row3">
+                <div className="boxes"></div>
+                <div className="boxes"></div>
+                <div className="boxes"></div>
+            </div>
+        </div>
+        <button className="reset">Reset</button>
+    </div>
+  )
+}
+
+export default JogoDaVelha;
